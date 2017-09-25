1c014af2056f8260dccbb7ae010a23d9aad35095
Michael Howard <mikenike@Michaels-MacBook-Pro.local>
2017-09-25 14:29:43 -0700

    needs if/else statements for birthday

---
+++ js1/day2/js/app.js
@@ -0,0 +1,5 @@
+var first = prompt("Whats is your first name?");
+var last = prompt("What is your last name?");
+console.log("First name: " + first);
+alert("Last name: " + last);
+var birthday = confirm("Is your birthday today?");
